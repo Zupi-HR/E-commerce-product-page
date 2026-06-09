a1e2729422639c38b4d28afb309dbbc9069f44e1
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2026-06-09 12:49:27 +0200

    feat: complete mobile menu navigation

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 const cartBtn = document.querySelector(".header__cart-btn");
+const body = document.querySelector("body");
+const openMenuBtn = document.querySelector(".header__menu-btn");
+const closeMenuBtn = document.getElementById("close-menu-btn");
+const overlay = document.getElementById("menu-overlay");
+
+function closeMenuButton() {
+  body.classList.remove("menu-is-open");
+  openMenuBtn.setAttribute("aria-expanded", "false");
+}
+
+function openMenuButton() {
+  body.classList.add("menu-is-open");
+  openMenuBtn.setAttribute("aria-expanded", "true");
+}
+
+openMenuBtn.addEventListener("click", openMenuButton);
+
+closeMenuBtn.addEventListener("click", closeMenuButton);
+
+overlay.addEventListener("click", closeMenuButton);
 
 cartBtn.addEventListener("click", () => {
   const isExpanded = cartBtn.getAttribute("aria-expanded");
