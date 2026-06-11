d279c61bee4b191eb4d4867297e473760129d52f
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2026-06-11 10:35:14 +0200

    Add mobile menu and image gallery functionality

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const body = document.querySelector("body");
 const openMenuBtn = document.querySelector(".header__menu-btn");
 const closeMenuBtn = document.getElementById("close-menu-btn");
 const overlay = document.getElementById("menu-overlay");
+const mainImg = document.querySelector(".main-image img");
+const prevImgBtn = document.querySelector(".main-image__btn--prev");
+const nextImgBtn = document.querySelector(".main-image__btn--next");
+let currentIndex = 0;
+
+const images = [
+  "images/image-product-1.jpg",
+  "images/image-product-2.jpg",
+  "images/image-product-3.jpg",
+  "images/image-product-4.jpg",
+];
 
 function closeMenuButton() {
   body.classList.remove("menu-is-open");
@@ -25,4 +36,16 @@ cartBtn.addEventListener("click", () => {
 
   if (isExpanded === "false") cartBtn.setAttribute("aria-expanded", "true");
   else cartBtn.setAttribute("aria-expanded", "false");
+});
+
+prevImgBtn.addEventListener("click", () => {
+  currentIndex--;
+  if (currentIndex < 0) currentIndex = 3;
+  mainImg.src = images[currentIndex];
+});
+
+nextImgBtn.addEventListener("click", () => {
+  currentIndex++;
+  if (currentIndex > 3) currentIndex = 0;
+  mainImg.src = images[currentIndex];
 });
