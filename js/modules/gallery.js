096f9f2a774e2fc340c1c9b7ce00b611c1d37b62
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2026-07-11 18:56:38 +0200

    feat: implement state-driven gallery view and thumbnail selection

--- js/modules/gallery.js
+++ js/modules/gallery.js
@@ -4,20 +4,15 @@ const nextImgBtn = document.querySelector(".main-image__btn--next");
 const thumbnailsContainer = document.querySelector(".images");
 let currentIndex = 0;
 
-const images = [
-  "images/image-product-1.jpg",
-  "images/image-product-2.jpg",
-  "images/image-product-3.jpg",
-  "images/image-product-4.jpg",
-];
-
 export function initGallery() {
   prevImgBtn.addEventListener("click", () => {
     currentIndex--;
+    updateGalleryView(currentIndex);
   });
 
   nextImgBtn.addEventListener("click", () => {
     currentIndex++;
+    updateGalleryView(currentIndex);
   });
 
   thumbnailsContainer.addEventListener("click", updateActiveThumbnail);
@@ -25,12 +20,11 @@ export function initGallery() {
   function updateActiveThumbnail(event) {
     const clickedThumbnail = event.target.closest("button");
     if (!clickedThumbnail) return;
-    thumbnailsContainer
-      .querySelector("[aria-current]")
-      ?.removeAttribute("aria-current");
-
-    clickedThumbnail.setAttribute("aria-current", "true");
-    updateMainImage(clickedThumbnail.dataset.image);
+    let newIndex = Array.from(
+      thumbnailsContainer.querySelectorAll("button"),
+    ).indexOf(clickedThumbnail);
+    currentIndex = newIndex;
+    updateGalleryView(currentIndex);
   }
 
   function updateMainImage(imgSrc) {
@@ -39,9 +33,19 @@ export function initGallery() {
 
   function updateGalleryView(index) {
     if (index < 0) index = 3;
-    mainImg.src = images[index];
-  }
 
-  if (currentIndex > 3) currentIndex = 0;
-  mainImg.src = images[currentIndex];
+    if (index > 3) index = 0;
+
+    thumbnailsContainer
+      .querySelector("[aria-current]")
+      ?.removeAttribute("aria-current");
+
+    const activeThumbnail = Array.from(
+      thumbnailsContainer.querySelectorAll("button"),
+    ).at(index);
+
+    activeThumbnail.setAttribute("aria-current", "true");
+    updateMainImage(activeThumbnail.dataset.image);
+    currentIndex = index;
+  }
 }
