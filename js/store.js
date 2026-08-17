9181136611fdf7aa2292d55bce4c03cc9e1898c3
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2026-08-17 13:33:49 +0200

    wip(store): add initial cart state and merge item quantities

--- js/store.js
+++ js/store.js
@@ -1,4 +1,5 @@
 let selectedQuantity = 0;
+const cartItems = [];
 
 export function updateQuantity(newValue) {
   if (Number.isInteger(newValue)) {
@@ -14,4 +15,16 @@ export function updateQuantity(newValue) {
 
 export function getSelectedQuantity() {
   return selectedQuantity;
+}
+
+export function addToCart(item) {
+  const existingItem = cartItems.find((element) => {
+    return item.id === element.id;
+  });
+
+  if (existingItem !== undefined) {
+    existingItem.quantity += item.quantity;
+  } else {
+    cartItems.push(item);
+  }
 }
