669e2c2e1611d4e94e4cc69afaec2e214b13884b
Zupi <49796750+Zupi-HR@users.noreply.github.com>
2026-09-01 11:50:42 +0200

    wip(cart): add cart state rendering and item template cloning

--- js/modules/cart.js
+++ js/modules/cart.js
@@ -1,4 +1,9 @@
-import { updateQuantity, getSelectedQuantity, addToCart } from "../store.js";
+import {
+  updateQuantity,
+  getSelectedQuantity,
+  addToCart,
+  getCart,
+} from "../store.js";
 import { product } from "../data/product.js";
 
 export function initCart() {
@@ -8,6 +13,12 @@ export function initCart() {
   const productQuantityInput = document.querySelector('input[type="number"]');
   const addToCartForm = document.querySelector(".add-to-cart-form");
   const cartQuantity = document.querySelector(".cart-quantity");
+  const emptyCart = document.querySelector(".empty-cart");
+  const cartItemsList = document.querySelector(".cart-items-list");
+  const checkoutBtn = document.querySelector(".checkout-btn");
+  const templateListEl = document.getElementById("cart-filled-template");
+
+  renderCartState(getCart());
 
   cartBtn.addEventListener("click", () => {
     const isExpanded = cartBtn.getAttribute("aria-expanded");
@@ -21,8 +32,7 @@ export function initCart() {
   });
 
   window.addEventListener("cart:updated", (e) => {
-    const cartItems = e.detail;
-    cartQuantity.textContent = cartItems[0].quantity;
+    renderCartState(e.detail);
   });
 
   productQuantityInput.addEventListener("input", (e) => {
@@ -45,11 +55,6 @@ export function initCart() {
     }
   });
 
-  function adjustQuantity(delta) {
-    const requestedQuantity = getSelectedQuantity() + delta;
-    updateQuantity(requestedQuantity);
-  }
-
   decrementBtn.addEventListener("click", () => {
     adjustQuantity(-1);
   });
@@ -66,4 +71,39 @@ export function initCart() {
     cartItem.thumbnail = images[0].thumbnail;
     addToCart(cartItem);
   });
+
+  function adjustQuantity(delta) {
+    const requestedQuantity = getSelectedQuantity() + delta;
+    updateQuantity(requestedQuantity);
+  }
+
+  function renderCartState(cartItems) {
+    if (cartItems.length === 0) {
+      emptyCart.style.display = "flex";
+      cartQuantity.style.display = "none";
+      cartItemsList.style.display = "none";
+      checkoutBtn.style.display = "none";
+    } else {
+      renderCartItems(cartItems);
+      emptyCart.style.display = "none";
+      cartQuantity.style.display = "flex";
+      cartItemsList.style.display = "block";
+      cartQuantity.textContent = cartItems[0].quantity;
+      checkoutBtn.style.display = "block";
+    }
+  }
+
+  function renderCartItems(cartItems) {
+    cartItemsList.replaceChildren();
+    cartItems.forEach((item) => {
+      const listItem = templateListEl.content.cloneNode(true);
+      listItem.querySelector(".cart-item-img").src = item.images[0].thumbnail;
+      listItem.querySelector(".cart-item-title").textContent = item.title;
+      listItem.querySelector(".price-value").textContent = item.unitPrice;
+      listItem.querySelector(".quantity-value").textContent = item.quantity;
+      listItem.querySelector(".total-value").textContent =
+        `${item.unitPrice * item.quantity}`;
+      cartItemsList.appendChild(listItem);
+    });
+  }
 }
